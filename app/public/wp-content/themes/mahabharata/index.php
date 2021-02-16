ac5380eb73bc972b97576c4f8a8589630e048cdb
ryu <dragonogawaz@gmail.com>
2021-02-16 13:22:23 +0900

    cssファイルの読み込み設定追加

--- app/public/wp-content/themes/mahabharata/index.php
+++ app/public/wp-content/themes/mahabharata/index.php
@@ -23,7 +23,7 @@
       href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@700&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" />
   </head>
   <body>
     <header class="header">
