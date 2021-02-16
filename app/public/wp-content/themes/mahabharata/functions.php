382511fead55b3e1dfe546e8dd0573fd76359ac0
ryu <dragonogawaz@gmail.com>
2021-02-16 20:51:07 +0900

    jsファイルをwp_footerで読み込み

--- app/public/wp-content/themes/mahabharata/functions.php
+++ app/public/wp-content/themes/mahabharata/functions.php
@@ -1,7 +1,7 @@
 <?php
 function add_file() {
     wp_enqueue_style('styles', get_template_directory_uri().'/css/styles.css');
-    wp_enqueue_script('animation_js', get_template_directory_uri().'/js/scroll-animation.js', array('jquery'));
+    wp_enqueue_script('scroll-animation', get_template_directory_uri().'/js/scroll-animation.js', array('jquery'), false, true);
 }
 
 add_action('wp_enqueue_scripts', 'add_file');
