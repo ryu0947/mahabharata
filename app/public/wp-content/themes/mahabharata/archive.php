1913aff9358d3b20c6793632b6ddc963119c6b7d
ryu <dragonogawaz@gmail.com>
2021-02-17 15:36:04 +0900

    アーカイブページ追加

---
+++ app/public/wp-content/themes/mahabharata/archive.php
@@ -0,0 +1,160 @@
+ <?php get_header(); ?>
+    <main>
+      <section class="news js-animation">
+        <div class="container outer">
+          <h2 class="news__title">NEWS</h2>
+          <div class="news__list">
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news1.jpg"
+                      alt="「完全版マハーバーラタ」2020年7月・東京講演！！"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">
+                      「完全版マハーバーラタ」2020年7月・東京講演！！
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news2.jpg"
+                      alt="ニュースのタイトルが入ります"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">ニュースのタイトルが入ります</p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news3.jpg"
+                      alt="ニュースのタイトルが入ります"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">ニュースのタイトルが入ります</p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news1.jpg"
+                      alt="「完全版マハーバーラタ」2020年7月・東京講演！！"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">
+                      「完全版マハーバーラタ」2020年7月・東京講演！！
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news2.jpg"
+                      alt="ニュースのタイトルが入ります"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">ニュースのタイトルが入ります</p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news3.jpg"
+                      alt="ニュースのタイトルが入ります"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">ニュースのタイトルが入ります</p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news1.jpg"
+                      alt="「完全版マハーバーラタ」2020年7月・東京講演！！"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">
+                      「完全版マハーバーラタ」2020年7月・東京講演！！
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news2.jpg"
+                      alt="ニュースのタイトルが入ります"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">ニュースのタイトルが入ります</p>
+                  </div>
+                </div>
+              </a>
+            </article>
+            <article class="news__item">
+              <a href="#">
+                <div class="news__item-inner">
+                  <div class="news__img">
+                    <img
+                      src="<?php echo get_template_directory_uri(); ?>/images/news3.jpg"
+                      alt="ニュースのタイトルが入ります"
+                    />
+                  </div>
+                  <div class="news__content">
+                    <time class="news__time">2019.11.30</time>
+                    <p class="news__text">ニュースのタイトルが入ります</p>
+                  </div>
+                </div>
+              </a>
+            </article>
+          </div>
+        </div>
+      </section>
+    <?php get_footer(); ?>
