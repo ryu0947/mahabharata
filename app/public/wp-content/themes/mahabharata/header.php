02dd6226df8a63442bc57be403a6c0e9f4c57cd5
ryu <dragonogawaz@gmail.com>
2021-02-17 09:45:07 +0900

    ページ別に条件分岐

--- app/public/wp-content/themes/mahabharata/header.php
+++ app/public/wp-content/themes/mahabharata/header.php
@@ -49,6 +49,7 @@
           </li>
         </ul>
       </nav>
+      <?php if(is_front_page() ): ?>
       <div class="fv">
         <div class="container">
           <div class="fv__inner">
@@ -76,4 +77,66 @@
           <a href="#">チケット予約サイトへ</a>
         </button>
       </div>
+    <?php elseif(is_page('story') ): ?>
+        <div class="fv-child story">
+        <div class="container">
+          <div class="fv-child__inner">
+            <div class="fv-child__flex">
+              <div class="fv-child__row">
+                <p class="fv-child__desc">
+                  世界最長の叙事詩をピーター・ブルック以来の全編舞台化
+                </p>
+                <h2 class="fv-child__title">完全版マハーバーラタ</h2>
+              </div>
+              <div class="fv-child__row">
+                <p class="fv-child__info">2020年7月4日〜7日</p>
+                <p class="fv-child__info">なかのZERO大ホール</p>
+              </div>
+            </div>
+            <div class="btn-position">
+              <button class="fv-child__btn--story">
+                <a href="#">チケット予約サイトへ</a>
+              </button>
+            </div>
+            <ul class="breadcrumb__list">
+              <li class="breadcrumb__item--story">
+                <a href="index.html">ホーム</a><span>&gt;</span>
+              </li>
+              <li class="breadcrumb__item--story">STORY</li>
+            </ul>
+            <div class="fv-child__intro js-animation">
+              <h1 class="fv-child__heading">STORY</h1>
+              <p class="fv-child__lead">
+                マハー（偉大な）、バーラタ（バラタ族）、つまり「偉大なバラタ族」の物語。神々が人間界を作り出すところから始まり、バラタ族の中のクル家とパーンドゥ家という二つの部族間の対立を巡るストーリー。神の血を引く個性豊かな登場人物たちによる差別、対立、欲望、嫉妬など、生の苦しみが描かれ、最終的には一族が破滅していく。
+                世界的な文学作品古代ギリシャの「イーリアス」「オデュッセイア」と並ぶ世界三大叙事詩の一つに数えられており、「ラーマーヤナ」と双璧を成すインド二大叙事詩の一つ。原語はサンスクリット語であるが、全18巻、10万詩節を超えるその長さは聖書の約4倍と言われ、原典の日本語訳はいまだ完結していない。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php else: ?>
+      <div class="fv-child">
+        <div class="container">
+          <div class="fv-child__inner">
+            <div class="fv-child__flex">
+              <div class="fv-child__row">
+                <p class="fv-child__desc">
+                  世界最長の叙事詩をピーター・ブルック以来の全編舞台化
+                </p>
+                <h2 class="fv-child__title">完全版マハーバーラタ</h2>
+              </div>
+              <div class="fv-child__row">
+                <p class="fv-child__info">2020年7月4日〜7日</p>
+                <p class="fv-child__info">なかのZERO大ホール</p>
+              </div>
+            </div>
+            <div class="btn-position">
+              <button class="fv-child__btn">
+                <a href="#">チケット予約サイトへ</a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+     <?php endif; ?>
     </header>
