f2e5a5c1ff99d8a4ca994556a60a3f43740f0bb3
ryu <dragonogawaz@gmail.com>
2021-02-22 10:42:33 +0900

    繰り返しフィールド用のコード追加

--- app/public/wp-content/themes/mahabharata/page-story.php
+++ app/public/wp-content/themes/mahabharata/page-story.php
@@ -1,32 +1,39 @@
   <?php get_header(); ?>
     <main>
       <section class="outer story">
-        <article class="synopsis synopsis-bg1 js-animation">
-          <div class="container">
-            <div class="synopsis__left">
-              <p class="synopsis__text">
-                <?php the_field('synopsis1'); ?>
-              </p>
+         <?php 
+            $filed_group = SCF::get('synopsis');
+            echo count ($filed_group);
+              foreach($filed_group as $filed) {
+               $img = wp_get_attachment_image_src($filed['synopsis-img'], 'full');
+         ?>
+          <article class="synopsis js-animation" style="background: url(<?php echo $img[0]; ?>)no-repeat center/cover;">
+            <div class="container">
+              <div class="synopsis__left">
+                <p class="synopsis__text">
+                <?php echo $filed['synopsis-text']; ?>
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
-        <article class="synopsis-bg2 js-animation">
-          <div class="container">
-            <div class="synopsis__right">
-              <p class="synopsis__text">
-              <?php the_field('synopsis2'); ?>
-              </p>
+          </article>
+          <article class="synopsis-bg2 js-animation">
+            <div class="container">
+              <div class="synopsis__right">
+                <p class="synopsis__text">
+                <?php the_field('synopsis2'); ?>
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
-        <article class="synopsis-bg3 js-animation">
-          <div class="container">
-            <div class="synopsis__left">
-              <p class="synopsis__text">
-              <?php the_field('synopsis3'); ?>
-              </p>
+          </article>
+          <article class="synopsis-bg3 js-animation">
+            <div class="container">
+              <div class="synopsis__left">
+                <p class="synopsis__text">
+                <?php the_field('synopsis3'); ?>
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+       <?php } ?>
       </section>
     <?php get_footer(); ?>
