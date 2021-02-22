554a3cdf6b0c1ef29565386c67273993564c6939
ryu <dragonogawaz@gmail.com>
2021-02-23 05:52:23 +0900

    ストーリーページインデント

--- app/public/wp-content/themes/mahabharata/page-story.php
+++ app/public/wp-content/themes/mahabharata/page-story.php
@@ -4,9 +4,9 @@
          <?php 
             $filed_group = SCF::get('synopsis');
               foreach($filed_group as $filed) {
-               $img1 = wp_get_attachment_image_src($filed['synopsis-img1'], 'full');
-               $img2 = wp_get_attachment_image_src($filed['synopsis-img2'], 'full');
-               $img3 = wp_get_attachment_image_src($filed['synopsis-img3'], 'full');
+                $img1 = wp_get_attachment_image_src($filed['synopsis-img1'], 'full');
+                $img2 = wp_get_attachment_image_src($filed['synopsis-img2'], 'full');
+                $img3 = wp_get_attachment_image_src($filed['synopsis-img3'], 'full');
          ?>
           <article class="synopsis js-animation" style="background: url(<?php echo $img1[0]; ?>)no-repeat center/cover;">
             <div class="container">
