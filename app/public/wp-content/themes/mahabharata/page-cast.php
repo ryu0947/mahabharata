ad2a88972925bb9b3d5363fd526102b6cd1a86ab
ryu <dragonogawaz@gmail.com>
2021-02-22 18:04:52 +0900

    サブキャスト一覧繰り返しフィールド追加

--- app/public/wp-content/themes/mahabharata/page-cast.php
+++ app/public/wp-content/themes/mahabharata/page-cast.php
@@ -7,21 +7,21 @@
             <ul class="cast__list">
                 <?php 
                   $cast_group = SCF::get("cast");
-                  foreach($cast_group as $filed_name => $filed_value){ 
-                    $img = wp_get_attachment_image_src($filed_value['cast-img'], 'full');
+                  foreach($cast_group as $cast_name => $cast_value){ 
+                    $img = wp_get_attachment_image_src($cast_value['cast-img'], 'full');
                 ?>
               <li class="cast__item">
-                <img src="<?php echo $img[0]; ?>" alt="<?php echo $filed_value['cast-name']; ?>" class="cast__img" />
+                <img src="<?php echo $img[0]; ?>" alt="<?php echo $cast_value['cast-name']; ?>" class="cast__img" />
                 <div class="cast__item-inner">
                   <div class="cast__info">
-                    <p class="cast__part"><?php echo $filed_value['cast-part']; ?></p>
-                    <h3 class="cast__name"><?php echo $filed_value['cast-name']; ?></h3>
+                    <p class="cast__part"><?php echo $cast_value['cast-part']; ?></p>
+                    <h3 class="cast__name"><?php echo $cast_value['cast-name']; ?></h3>
                     <p class="cast__job">
-                      <?php echo $filed_value['cast-job']; ?>
+                      <?php echo $cast_value['cast-job']; ?>
                     </p>
                   </div>
                   <p class="cast__text">
-                    <?php echo nl2br($filed_value['cast-intro']); ?>
+                    <?php echo nl2br($cast_value['cast-intro']); ?>
                   </p>
                 </div>
               </li>
@@ -32,90 +32,25 @@
         <div class="cast__area js-animation">
           <div class="container">
             <ul class="cast__sub-list">
+            <?php 
+                  $subCast_group = SCF::get("sub-cast");
+                  foreach($subCast_group as $subCast_name => $subCast_value){ 
+                    $img = wp_get_attachment_image_src($subCast_value['sub-cast-img'], 'full');
+                ?>
               <li class="cast__sub-item">
-                <img
-                  src="images/cast2.jpg"
-                  alt="白井さち子"
-                  class="cast__sub-img"
-                />
+              <img src="<?php echo $img[0]; ?>" alt="<?php echo $subCast_value['sub-cast-name']; ?>" class="cast__sub-img" />
                 <div class="cast__sub-item-inner">
                   <div class="cast__info">
-                    <p class="cast__part">出演</p>
-                    <h3 class="cast__name">白井さち子</h3>
-                    <p class="cast__job">(バレエ、コンテンポラリーダンス)</p>
+                    <p class="cast__part"><?php echo $subCast_value['sub-cast-part']; ?></p>
+                    <h3 class="cast__name"><?php echo $subCast_value['sub-cast-name']; ?></h3>
+                    <p class="cast__job"><?php echo $subCast_value['subcast-job']; ?></p>
                   </div>
                   <p class="cast__text--center">
-                    7歳よりクラシックバレエを始める。<br />
-                    82年より6年間橘バレエ学校に在籍。<br />
-                    牧阿佐美に師事。<br />
-                    日本女子体育短期大学舞踊コース卒。<br />
-                    在籍中、太田順造にパントマイムを師事。
+                    <?php echo nl2br($subCast_value['sub-cast-intro']); ?>
                   </p>
                 </div>
               </li>
-              <li class="cast__sub-item">
-                <img
-                  src="images/cast2.jpg"
-                  alt="白井さち子"
-                  class="cast__sub-img"
-                />
-                <div class="cast__sub-item-inner">
-                  <div class="cast__info">
-                    <p class="cast__part">出演</p>
-                    <h3 class="cast__name">白井さち子</h3>
-                    <p class="cast__job">(バレエ、コンテンポラリーダンス)</p>
-                  </div>
-                  <p class="cast__text--center">
-                    7歳よりクラシックバレエを始める。<br />
-                    82年より6年間橘バレエ学校に在籍。<br />
-                    牧阿佐美に師事。<br />
-                    日本女子体育短期大学舞踊コース卒。<br />
-                    在籍中、太田順造にパントマイムを師事。
-                  </p>
-                </div>
-              </li>
-              <li class="cast__sub-item">
-                <img
-                  src="images/cast2.jpg"
-                  alt="白井さち子"
-                  class="cast__sub-img"
-                />
-                <div class="cast__sub-item-inner">
-                  <div class="cast__info">
-                    <p class="cast__part">出演</p>
-                    <h3 class="cast__name">白井さち子</h3>
-                    <p class="cast__job">(バレエ、コンテンポラリーダンス)</p>
-                  </div>
-                  <p class="cast__text--center">
-                    7歳よりクラシックバレエを始める。<br />
-                    82年より6年間橘バレエ学校に在籍。<br />
-                    牧阿佐美に師事。<br />
-                    日本女子体育短期大学舞踊コース卒。<br />
-                    在籍中、太田順造にパントマイムを師事。
-                  </p>
-                </div>
-              </li>
-              <li class="cast__sub-item">
-                <img
-                  src="images/cast2.jpg"
-                  alt="白井さち子"
-                  class="cast__sub-img"
-                />
-                <div class="cast__sub-item-inner">
-                  <div class="cast__info">
-                    <p class="cast__part">出演</p>
-                    <h3 class="cast__name">白井さち子</h3>
-                    <p class="cast__job">(バレエ、コンテンポラリーダンス)</p>
-                  </div>
-                  <p class="cast__text--center">
-                    7歳よりクラシックバレエを始める。<br />
-                    82年より6年間橘バレエ学校に在籍。<br />
-                    牧阿佐美に師事。<br />
-                    日本女子体育短期大学舞踊コース卒。<br />
-                    在籍中、太田順造にパントマイムを師事。
-                  </p>
-                </div>
-              </li>
+             <?php } ?>
             </ul>
           </div>
         </div>
