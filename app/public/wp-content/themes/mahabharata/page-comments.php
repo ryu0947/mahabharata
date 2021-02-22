28bc79e5ebce9f4146d3b4b346798c17a10fdbda
ryu <dragonogawaz@gmail.com>
2021-02-23 07:16:52 +0900

    カスタムフィールドで設定した値を出力

--- app/public/wp-content/themes/mahabharata/page-comments.php
+++ app/public/wp-content/themes/mahabharata/page-comments.php
@@ -6,20 +6,14 @@
           <div class="comments__caption">
             <?php echo get_the_content(); ?>
           </div>
-          <div class="comments__inner" style="background: url(<?php the_field('top-comments-img'); ?>)no-repeat center/cover;">
+          <div class="comments__inner" style="background: url(<?php the_field('top-comments-img'); ?>)no-repeat 75% center / cover;">
             <div class="comments__text-area">
               <h3 class="comments__heading">
-                京都佛立ミュージアム館長<br />京都・長松寺/横浜・妙深寺<br
-                  class="sp"
-                />住職
-                <strong class="comments__strong">長松清潤</strong>
+              <?php the_field('representative-job'); ?><br class="sp" />
+                <strong class="comments__strong"><?php the_field('representative-name'); ?></strong>
               </h3>
               <p class="comments__text">
-                「文に非ず、其の義に非ず、唯だ一部の意のみ。」<br />
-                まずこの聖句が浮かんだ。境界線に立つ人類。超越する意志。小池博史氏の心象が生み<br />
-                出したアバターが乱舞しながら深層意識に波紋を起こしてゆく。<br />
-                仏教の奥義と小池作品が通底して見えるのは何故か。<br />
-                40年に及ぶ創作活動で到達した究極の舞台が普遍の題材により現出されようとしている。<br />
+                <?php the_field('top-comments'); ?>
               </p>
               <div class="btn-position--comments">
                 <button class="comments__btn">
@@ -29,116 +23,6 @@
             </div>
           </div>
           <ul class="comments__list">
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
-            <li class="comments__item">
-              <div class="comments__info">
-                <h3 class="comments__name">今福龍太</h3>
-                <p class="comments__job">文化人類学者</p>
-              </div>
-              <p class="comments__text--black">
-                蝶は風を受けて飛ぶ。いやむしろ、風を切って飛ぶ。いやさらにいえば、みずから風を孕み風を生み出す。
-                吹きすさび循環する大風のような物語。小池舞台はそんな物語を生成する未知なる蝶の飛翔だ。
-              </p>
-            </li>
             <li class="comments__item">
               <div class="comments__info">
                 <h3 class="comments__name">今福龍太</h3>
