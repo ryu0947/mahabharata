c6c2932a054b3e6f7d5a4d3711c025fa6580897b
ryu <dragonogawaz@gmail.com>
2021-02-24 07:01:14 +0900

    プラグイン導入につきHTML削除

--- app/public/wp-content/themes/mahabharata/page-inquiry.php
+++ app/public/wp-content/themes/mahabharata/page-inquiry.php
@@ -5,35 +5,6 @@
           <h2 class="inquiry__title"><?php echo get_the_title(); ?></h2>
           <div class="inquiry__inner">
               <?php the_content(); ?>
-            <form action="#">
-              <dl class="form">
-                <div class="form__row">
-                  <dt class="form__text">お名前</dt>
-                  <dd class="form__area">
-                    <input type="text" name="name" />
-                  </dd>
-                </div>
-                <div class="form__row">
-                  <dt class="form__text">お電話番号</dt>
-                  <dd class="form__area">
-                    <input type="tel" name="tel" />
-                  </dd>
-                </div>
-                <div class="form__row">
-                  <dt class="form__text">メールアドレス</dt>
-                  <dd class="form__area">
-                    <input type="email" name="email" />
-                  </dd>
-                </div>
-                <div class="form__row">
-                  <dt class="form__text">内容</dt>
-                  <dd class="form__area">
-                    <textarea name="message" cols="40" rows="10"></textarea>
-                  </dd>
-                </div>
-              </dl>
-              <input type="button" value="送信" class="submit-btn" />
-            </form>
           </div>
         </div>
       </section>
