e4c575eb6c0d3f60d37b52c94a4eb095a2cefa8b
ryu <dragonogawaz@gmail.com>
2021-02-25 06:14:42 +0900

    テキストのスタイリング

--- app/public/wp-content/themes/mahabharata/page-confirm.php
+++ app/public/wp-content/themes/mahabharata/page-confirm.php
@@ -3,7 +3,7 @@
       <section class="inquiry">
         <div class="container outer">
           <h2 class="inquiry__title"><?php echo get_the_title(); ?></h2>
-          <div class="inquiry__inner">
+          <div class="inquiry__inner inquiry__center">
             <?php the_content(); ?>
           </div>
         </div>
