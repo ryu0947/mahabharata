8148f78d4c2a6fda56359ac04f6a0c44e4f1491d
ryu <dragonogawaz@gmail.com>
2021-02-26 06:52:57 +0900

    404エラーページにトップへ戻るリンク追加

--- app/public/wp-content/themes/mahabharata/404.php
+++ app/public/wp-content/themes/mahabharata/404.php
@@ -2,7 +2,8 @@
     <main>
       <section class="error js-animation">
         <div class="container outer">
-          <h2 class="error__title"><?php echo get_the_title(); ?></h2>
+          <h2 class="error__title">ページが見つかりません</h2>
+          <p class="error__text"><a href="<?php echo esc_url(home_url('/')); ?> ">トップページへ戻る</a></p>
         </div>
       </section>
     <?php get_footer(); ?>
