95eece63e84bd4d435d4d65a7b83f74ebd325095
ryu <dragonogawaz@gmail.com>
2021-02-26 05:46:10 +0900

    404エラーページ作成

---
+++ app/public/wp-content/themes/mahabharata/404.php
@@ -0,0 +1,8 @@
+  <?php get_header(); ?>
+    <main>
+      <section class="inquiry js-animation">
+        <div class="container outer">
+          <h2 class="inquiry__title"><?php echo get_the_title(); ?></h2>
+        </div>
+      </section>
+    <?php get_footer(); ?>
