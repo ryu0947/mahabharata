75ca2875efb12acff5fabfa3607f1c07edef7f5b
ryu <dragonogawaz@gmail.com>
2021-02-27 09:52:42 +0900

    トップページニュース一覧

--- app/public/wp-content/themes/mahabharata/front-page.php
+++ app/public/wp-content/themes/mahabharata/front-page.php
@@ -28,88 +28,33 @@
         <div class="container outer">
           <h2 class="news__title">NEWS</h2>
           <div class="news__list top">
+              <?php 
+                $news_posts = get_post_pages();
+              
+                if($news_posts -> have_posts()):
+                  while($news_posts -> have_posts()):
+                    $news_posts -> the_post();
+              ?>
             <article class="news__item">
-              <a href="#">
+              <a href="<?php the_permalink(); ?>">
                 <div class="news__item-inner">
                   <div class="news__img">
-                    <img
-                      src="<?php echo get_template_directory_uri(); ?>/images/news1.jpg"
-                      alt="「完全版マハーバーラタ」2020年7月・東京講演！！"
-                    />
+                  <?php the_post_thumbnail('news'); ?>
                   </div>
                   <div class="news__content">
-                    <time class="news__time">2019.11.30</time>
+                    <time class="news__time"><?php the_time('Y.m.d'); ?></time>
                     <p class="news__text">
-                      「完全版マハーバーラタ」2020年7月・東京講演！！
+                    <?php the_title(); ?>
                     </p>
                   </div>
                 </div>
               </a>
             </article>
-            <article class="news__item">
-              <a href="#">
-                <div class="news__item-inner">
-                  <div class="news__img">
-                    <img
-                      src="<?php echo get_template_directory_uri(); ?>/images/news2.jpg"
-                      alt="ニュースのタイトルが入ります"
-                    />
-                  </div>
-                  <div class="news__content">
-                    <time class="news__time">2019.11.30</time>
-                    <p class="news__text">ニュースのタイトルが入ります</p>
-                  </div>
-                </div>
-              </a>
-            </article>
-            <article class="news__item">
-              <a href="#">
-                <div class="news__item-inner">
-                  <div class="news__img">
-                    <img
-                      src="<?php echo get_template_directory_uri(); ?>/images/news3.jpg"
-                      alt="ニュースのタイトルが入ります"
-                    />
-                  </div>
-                  <div class="news__content">
-                    <time class="news__time">2019.11.30</time>
-                    <p class="news__text">ニュースのタイトルが入ります</p>
-                  </div>
-                </div>
-              </a>
-            </article>
-            <article class="news__item">
-              <a href="#">
-                <div class="news__item-inner">
-                  <div class="news__img">
-                    <img
-                      src="<?php echo get_template_directory_uri(); ?>/images/news2.jpg"
-                      alt="ニュースのタイトルが入ります"
-                    />
-                  </div>
-                  <div class="news__content">
-                    <time class="news__time">2019.11.30</time>
-                    <p class="news__text">ニュースのタイトルが入ります</p>
-                  </div>
-                </div>
-              </a>
-            </article>
-            <article class="news__item">
-              <a href="#">
-                <div class="news__item-inner">
-                  <div class="news__img">
-                    <img
-                      src="<?php echo get_template_directory_uri(); ?>/images/news2.jpg"
-                      alt="ニュースのタイトルが入ります"
-                    />
-                  </div>
-                  <div class="news__content">
-                    <time class="news__time">2019.11.30</time>
-                    <p class="news__text">ニュースのタイトルが入ります</p>
-                  </div>
-                </div>
-              </a>
-            </article>
+        <?php 
+           endwhile;
+          endif;
+         wp_reset_postdata();
+        ?>
           </div>
           <button class="news__btn"><a href="#">ニュース一覧へ</a></button>
         </div>
