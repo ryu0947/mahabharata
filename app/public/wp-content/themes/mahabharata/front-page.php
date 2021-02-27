9816c457f8493ef07523281066bb59ad85e8090d
ryu <dragonogawaz@gmail.com>
2021-02-27 17:19:29 +0900

    CASTエリアカスタムフィールドの出力

--- app/public/wp-content/themes/mahabharata/front-page.php
+++ app/public/wp-content/themes/mahabharata/front-page.php
@@ -106,76 +106,31 @@
         </div>
       </section>
       <section class="cast outer js-animation">
-        <h2 class="cast__title">CAST</h2>
+        <h2 class="cast__title"><?php echo get_the_title(19); ?></h2>
         <div class="cast__bg">
           <div class="container">
-            <ul class="cast__list">
+          <ul class="cast__list">
+                <?php 
+                  $cast_group = SCF::get("cast", 19);
+                  foreach($cast_group as $cast_name => $cast_value):
+                    $img = wp_get_attachment_image_src($cast_value['cast-img'], 'full');
+                ?>
               <li class="cast__item">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/cast1.jpg" alt="小池博史" class="cast__img" />
+                <img src="<?php echo $img[0]; ?>" alt="<?php echo $cast_value['cast-name']; ?>" class="cast__img" />
                 <div class="cast__item-inner">
                   <div class="cast__info">
-                    <p class="cast__part">作・演出・振付・構成</p>
-                    <h3 class="cast__name">小池博史</h3>
+                    <p class="cast__part"><?php echo $cast_value['cast-part']; ?></p>
+                    <h3 class="cast__name"><?php echo $cast_value['cast-name']; ?></h3>
                     <p class="cast__job">
-                      (演出家・作家・振付家・舞台美術家・写真家)
+                      <?php echo $cast_value['cast-job']; ?>
                     </p>
                   </div>
                   <p class="cast__text">
-                    茨城県日立市出身。一橋大学卒業。<br />
-                    演出家・作家・振付家・舞台美術家・写真家、舞台芸術の学校（P.A.I.）校長。<br />
-                    1982年「パパ・タラフマラ」設立。<br />
-                    演劇、舞踊、オペラ、美術、建築等、ジャンルを縦横に渡りながら空間を築き上げてゆく手法で、国際的に高い評価を確立。<br />
-                    3.11を受けて、翌2012年5月にパパ・タラフマラ解散。すぐに「小池博史ブリッジプロジェクト」を立ち上げ、作品を創作しながら、若手表現者の育成と芸術文化事業のプロデュースを手掛けるなど、活動は多岐に渡る。<br />
-                    著書に「ロンググッドバイ～パパ・タラフマラとその時代」（青幻舎刊）、「からだのこえをきく」（新潮社刊）等。
+                    <?php echo nl2br($cast_value['cast-intro']); ?>
                   </p>
                 </div>
               </li>
-              <li class="cast__item">
-                <img
-                  src="<?php echo get_template_directory_uri(); ?>/images/cast2.jpg"
-                  alt="白井さち子"
-                  class="cast__img"
-                />
-                <div class="cast__item-inner">
-                  <div class="cast__info">
-                    <p class="cast__part">出演</p>
-                    <h3 class="cast__name">白井さち子</h3>
-                    <p class="cast__job">(バレエ、コンテンポラリーダンス)</p>
-                  </div>
-                  <p class="cast__text">
-                    7歳よりクラシックバレエを始める。<br />
-                    82年より6年間橘バレエ学校に在籍。牧阿佐美に師事。<br />
-                    日本女子体育短期大学舞踊コース卒。<br />
-                    在籍中、太田順造にパントマイムを師事。<br />
-                    89年よりパパ・タラフマラに参加。以降国内外の公演に出演。<br />
-                    後身のパフォーマーのダンス指導にもあっている。<br />
-                    パパ・タラフマラ作品「シンデレラ」においてシンデレラ役、「三人姉妹」次女役、「パパ・タラフマラの白雪姫」継母役などを好演。<br />
-                    これまで約35カ国での公演に参加、そのパフォーマンスは世界各地で認められ、高く評価されている。
-                  </p>
-                </div>
-              </li>
-              <li class="cast__item">
-                <img
-                  src="<?php echo get_template_directory_uri(); ?>/images/cast3.jpg"
-                  alt="小谷野哲郎"
-                  class="cast__img"
-                />
-                <div class="cast__item-inner">
-                  <div class="cast__info">
-                    <p class="cast__part">出演</p>
-                    <h3 class="cast__name">小谷野哲郎</h3>
-                    <p class="cast__job">(バリ舞踏)</p>
-                  </div>
-                  <p class="cast__text">
-                    東海大学音楽学課程在学中よりサウンドスケープ研究の傍らバリ舞踊を始める。<br />
-                    1995年よりインドネシア政府給費留学生としてインドネシア国立芸術高等学院に留学。<br />
-                    学外でも現代バリ舞踊界最高の舞踊家達に師事し、舞踊技術と同時に精神的哲学的影響も強く受ける。<br />
-                    2006年、Asian Cultural
-                    Councilの助成によりアメリカに滞在。<br />
-                    パパ・タラフマラ作品では「ガリバー＆スウィフト」「パンク・ドンキホーテ」「白雪姫」に、小池博史ブリッジプロジェクトでは「注文の多い料理店」「風の又三郎」に出演。
-                  </p>
-                </div>
-              </li>
+             <?php endforeach; ?>
             </ul>
             <button class="cast__btn"><a href="<?php echo esc_url( home_url('cast')); ?>">もっと見る</a></button>
           </div>
