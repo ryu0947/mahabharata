a6ab7095d79ebd50cd492839bfcd05a31357400e
ryu <dragonogawaz@gmail.com>
2021-02-28 22:04:38 +0900

    カスタムフィールドに入力がなかったらタグを表示させない処理追加

--- app/public/wp-content/themes/mahabharata/page-story.php
+++ app/public/wp-content/themes/mahabharata/page-story.php
@@ -8,39 +8,51 @@
         $img2 = wp_get_attachment_image_src($filed['synopsis-img2'], 'full');
         $img3 = wp_get_attachment_image_src($filed['synopsis-img3'], 'full');
       ?>
-      <?php 
-      $img_filed = get_post_meta(44, 'synopsis-img1', true);
-      $text_filed = get_post_meta(44, 'synopsis-text1', true);
-      if($img_filed && $text_filed): 
-      ?>
-        <article class="synopsis js-animation" style="background: url(<?php echo $img1[0]; ?>)no-repeat center/cover;">
-          <div class="container">
-            <div class="synopsis__left">
-              <p class="synopsis__text">
-                <?php echo $filed['synopsis-text1']; ?>
-              </p>
+        <?php
+        $img_filed = get_post_meta(44, 'synopsis-img1', true);
+        $text_filed = get_post_meta(44, 'synopsis-text1', true);
+        if ($img_filed && $text_filed) :
+        ?>
+          <article class="synopsis js-animation" style="background: url(<?php echo $img1[0]; ?>)no-repeat center/cover;">
+            <div class="container">
+              <div class="synopsis__left">
+                <p class="synopsis__text">
+                  <?php echo $filed['synopsis-text1']; ?>
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
         <?php endif; ?>
-        <article class="js-animation" style="background: url(<?php echo $img2[0]; ?>)no-repeat center/cover;">
-          <div class="container">
-            <div class="synopsis__right">
-              <p class="synopsis__text">
-                <?php echo $filed['synopsis-text2']; ?>
-              </p>
+        <?php
+        $img_filed = get_post_meta(44, 'synopsis-img2', true);
+        $text_filed = get_post_meta(44, 'synopsis-text2', true);
+        if ($img_filed && $text_filed) :
+        ?>
+          <article class="js-animation" style="background: url(<?php echo $img2[0]; ?>)no-repeat center/cover;">
+            <div class="container">
+              <div class="synopsis__right">
+                <p class="synopsis__text">
+                  <?php echo $filed['synopsis-text2']; ?>
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
-        <article class="js-animation" style="background: url(<?php echo $img3[0]; ?>)no-repeat center/cover;">
-          <div class="container">
-            <div class="synopsis__left">
-              <p class="synopsis__text">
-                <?php echo $filed['synopsis-text3']; ?>
-              </p>
+          </article>
+        <?php endif; ?>
+        <?php
+        $img_filed = get_post_meta(44, 'synopsis-img3', true);
+        $text_filed = get_post_meta(44, 'synopsis-text3', true);
+        if ($img_filed && $text_filed) :
+        ?>
+          <article class="js-animation" style="background: url(<?php echo $img3[0]; ?>)no-repeat center/cover;">
+            <div class="container">
+              <div class="synopsis__left">
+                <p class="synopsis__text">
+                  <?php echo $filed['synopsis-text3']; ?>
+                </p>
+              </div>
             </div>
-          </div>
-        </article>
+          </article>
+        <?php endif; ?>
       <?php endforeach; ?>
     </section>
     <?php get_footer(); ?>
