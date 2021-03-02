7ca7ae333d7c3315dff6163d57a197c46d5aca5d
ryu <dragonogawaz@gmail.com>
2021-03-02 17:04:25 +0900

    公演スケジュールの繰り返しフィールドの入力チェック

--- app/public/wp-content/themes/mahabharata/footer.php
+++ app/public/wp-content/themes/mahabharata/footer.php
@@ -6,14 +6,22 @@
         <?php
         $schedule_group = SCF::get('schedule', 29);
         foreach ($schedule_group as $schedule) :
+          $day = get_post_meta(29, $schedule['day'], true);
+          $time = get_post_meta($schedule['time']);
+          $location = get_post_meta($schedule['location']);
+          $ticket = get_post_meta($schedule['ticket']);
+          if(empty($day && $time && $location && $ticket)):
         ?>
           <li class="schedule__item">
             <span class="schedule__text"><?php echo $schedule['day']; ?></span>
             <span class="schedule__text"><?php echo $schedule['time']; ?></span>
             <span class="schedule__text"><?php echo $schedule['location']; ?></span>
-            <span class="schedule__text"><a href="<?php the_field('reservation', 33); ?>" class="schedule__link">チケット予約受付中</a></span>
+            <span class="schedule__text"><a href="<?php the_field('reservation', 33); ?>" class="schedule__link"><?php echo $schedule['ticket']; ?></a></span>
           </li>
-        <?php endforeach; ?>
+        <?php 
+        endif;
+       endforeach; 
+       ?>
       </ul>
     </div>
     <div class="schedule_link-area">
