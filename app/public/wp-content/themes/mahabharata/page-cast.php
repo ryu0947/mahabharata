0e902b03c26e3f9c0195ffe5f5052acf3c1fc50b
ryu <dragonogawaz@gmail.com>
2021-03-03 11:49:59 +0900

    メインキャストエリアのカスタムフィールド未入力対策

--- app/public/wp-content/themes/mahabharata/page-cast.php
+++ app/public/wp-content/themes/mahabharata/page-cast.php
@@ -7,25 +7,33 @@
           <ul class="cast__list">
             <?php
             $cast_group = SCF::get("cast");
-            foreach ($cast_group as $cast_name => $cast_value) :
-              $img = wp_get_attachment_image_src($cast_value['cast-img'], 'full');
+            foreach ($cast_group as $cast) :
+              $img = wp_get_attachment_image_src($cast['cast-img'], 'full');
+              $name = get_post_meta(19, $cast['cast-name'], true);
+              $part = get_post_meta(19, $cast['cast-part'], true);
+              $job = get_post_meta(19, $cast['cast-job'], true);
+              $intro = get_post_meta(19, $cast['cast-intro'], true);
+
+              if (empty($img && $name && $part && $job && $intro)) :
             ?>
-              <li class="cast__item">
-                <img src="<?php echo $img[0]; ?>" alt="<?php echo $cast_value['cast-name']; ?>" class="cast__img" />
-                <div class="cast__item-inner">
-                  <div class="cast__info">
-                    <p class="cast__part"><?php echo $cast_value['cast-part']; ?></p>
-                    <h3 class="cast__name"><?php echo $cast_value['cast-name']; ?></h3>
-                    <p class="cast__job">
-                      <?php echo $cast_value['cast-job']; ?>
+                <li class="cast__item">
+                  <img src="<?php echo $img[0]; ?>" alt="<?php echo $cast['cast-name']; ?>" class="cast__img" />
+                  <div class="cast__item-inner">
+                    <div class="cast__info">
+                      <p class="cast__part"><?php echo $cast['cast-part']; ?></p>
+                      <h3 class="cast__name"><?php echo $cast['cast-name']; ?></h3>
+                      <p class="cast__job">
+                        <?php echo $cast['cast-job']; ?>
+                      </p>
+                    </div>
+                    <p class="cast__text">
+                      <?php echo nl2br($cast['cast-intro']); ?>
                     </p>
                   </div>
-                  <p class="cast__text">
-                    <?php echo nl2br($cast_value['cast-intro']); ?>
-                  </p>
-                </div>
-              </li>
-            <?php endforeach; ?>
+                </li>
+            <?php
+              endif;
+            endforeach; ?>
           </ul>
         </div>
       </div>
@@ -34,19 +42,19 @@
           <ul class="cast__sub-list">
             <?php
             $subCast_group = SCF::get("sub-cast");
-            foreach ($subCast_group as $subCast_name => $subCast_value) :
-              $img = wp_get_attachment_image_src($subCast_value['sub-cast-img'], 'full');
+            foreach ($subCast_group as $subCast) :
+              $img = wp_get_attachment_image_src($subCast['sub-cast-img'], 'full');
             ?>
               <li class="cast__sub-item">
-                <img src="<?php echo $img[0]; ?>" alt="<?php echo $subCast_value['sub-cast-name']; ?>" class="cast__sub-img" />
+                <img src="<?php echo $img[0]; ?>" alt="<?php echo $subCast['sub-cast-name']; ?>" class="cast__sub-img" />
                 <div class="cast__sub-item-inner">
                   <div class="cast__info">
-                    <p class="cast__part"><?php echo $subCast_value['sub-cast-part']; ?></p>
-                    <h3 class="cast__name"><?php echo $subCast_value['sub-cast-name']; ?></h3>
-                    <p class="cast__job"><?php echo $subCast_value['sub-cast-job']; ?></p>
+                    <p class="cast__part"><?php echo $subCast['sub-cast-part']; ?></p>
+                    <h3 class="cast__name"><?php echo $subCast['sub-cast-name']; ?></h3>
+                    <p class="cast__job"><?php echo $subCast['sub-cast-job']; ?></p>
                   </div>
                   <p class="cast__text--center">
-                    <?php echo nl2br($subCast_value['sub-cast-intro']); ?>
+                    <?php echo nl2br($subCast['sub-cast-intro']); ?>
                   </p>
                 </div>
               </li>
@@ -59,15 +67,15 @@
           <ul class="cast__text-list">
             <?php
             $credit_group = SCF::get("credit");
-            foreach ($credit_group as $credit_name => $credit_value) :
+            foreach ($credit_group as $credit) :
             ?>
               <li class="cast__text-item">
                 <div class="cast__item-inner">
                   <div class="cast__info">
-                    <p class="cast__part"><?php echo $credit_value['credit-part']; ?></p>
-                    <h3 class="cast__name"><?php echo $credit_value['credit-name']; ?></h3>
+                    <p class="cast__part"><?php echo $credit['credit-part']; ?></p>
+                    <h3 class="cast__name"><?php echo $credit['credit-name']; ?></h3>
                     <p class="cast__job">
-                      <?php echo $credit_value['credit-job']; ?>
+                      <?php echo $credit['credit-job']; ?>
                     </p>
                   </div>
                 </div>
