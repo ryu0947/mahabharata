07a758c77cf611c5e360454fc11891a526003f67
ryu <dragonogawaz@gmail.com>
2021-03-07 06:09:27 +0900

    throttleでスクロールイベント制御

--- app/public/wp-content/themes/mahabharata/js/scroll-animation.js
+++ app/public/wp-content/themes/mahabharata/js/scroll-animation.js
@@ -1,14 +1,16 @@
 jQuery(function ($) {
   const target = $(".js-animation");
-  $(window).on("scroll", function () {
-    $(target).each(function () {
-      const scroll = $(window).scrollTop();
-      const winHeight = $(window).height();
-      const targetHeight = $(this).offset().top;
+  $(window).scroll(
+    $.throttle(250, function () {
+      $(target).each(function () {
+        const scroll = $(window).scrollTop();
+        const winHeight = $(window).height();
+        const targetHeight = $(this).offset().top;
 
-      if (scroll > targetHeight - winHeight) {
-        $(this).addClass("show");
-      }
-    });
-  });
+        if (scroll > targetHeight - winHeight / 1.5) {
+          $(this).addClass("show");
+        }
+      });
+    })
+  );
 });
